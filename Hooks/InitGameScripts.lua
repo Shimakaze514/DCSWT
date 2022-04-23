be43a22554a4725e699f911321380cdcbc4bdb45
whirwind <450011209@163.com>
2022-04-23 16:04:05 +0800

    slotAuth

--- Hooks/InitGameScripts.lua
+++ Hooks/InitGameScripts.lua
@@ -1,4 +1,5 @@
 local loadVersion = "Version3.0"
 
 dofile(lfs.writedir() .. 'Scripts/ServerData/init.lua')
+dofile(lfs.writedir() .. 'Scripts/SlotAuth/SlotAuth.lua')
 dofile(lfs.writedir() .. "Scripts/LoadMissionScript/Config.lua")
