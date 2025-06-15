1dfbf38bd4a2f310be6a0d4c032b81c866a1e581
Shimakaze514 <shimakaze001@outlook.com>
2025-06-15 13:28:19 +0800

    添加SRS频率提示

--- Source/Version3.0/Mission/SourceSys.lua
+++ Source/Version3.0/Mission/SourceSys.lua
@@ -93,7 +93,7 @@ SourceObj.onBirth = function(_unit)
 end
 SourceObj.initMessage = function(_args)
   --trigger.action.outTextForGroup(_args[1], "指挥官玩家请保持SRS正常通联,呼叫无回应者暂时取消权限。", 60, true)
-  trigger.action.outTextForGroup(_args[1], "本服玩法需要团队配合，请您保持SRS无线电正常通联，以便于队友随时呼叫！", 60, true)
+  trigger.action.outTextForGroup(_args[1], "本服玩法需要团队配合，请您保持SRS无线电正常通联，以便于队友随时呼叫！\n频道列表：\n  ATC = 261.000\n  GCI = 124.800\n  公共频道 = 251.000（敌友都能听见！）", 60, true)
   local message = "*服务器已启用资源系统，请看下规则，避免起飞自爆"..'\n[1]服务器永久保存每位玩家的剩余资源点数,可通过F10菜单查询;\n[2]飞机、弹药、吊舱等都消耗资源点,起飞后扣除.返场降落将根据余量返点;\n[3]击杀敌方单位、吊运、救援，值班GCI、ATC、OP都可获取点数;\n[4]起飞前请检查"余额"及挂载量、合理支配点数，如果资源点不足以支付消耗，强行起飞将会自爆;\n[5]点数耗尽的话,每隔一段时间会发放低保点数;\n\n'.."你当前私有点数: " .. tostring(_args[2])
   trigger.action.outTextForGroup(_args[1], message, 60)
 end
