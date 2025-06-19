dc0176d17a8f1f93300d94f0bb655554d3645178
Shimakaze514 <shimakaze001@outlook.com>
2025-06-20 01:39:25 +0800

    debug箱子重量

--- Mission/CTLD.lua
+++ Mission/CTLD.lua
@@ -3462,9 +3462,9 @@ function ctld.getWeightOfCargo(unitName)
     if _crate then
         ctld.logTrace(string.format("_crate=%s", ctld.p(_crate)))
         --if _crate.simulatedSlingload then --! 手动把_crate.simulatedSlingload设置为true
-            ctld.logTrace(string.format("_crate.weight=%s", ctld.p(_crate.mass)))  --! 好像没有.weight但是有.mass
-            _weight = _weight + _crate.mass
-            _description = _description .. string.format("1 %s crate onboard (%s kg)\n", _crate.desc, _crate.mass)
+            ctld.logTrace(string.format("_crate.weight=%s", ctld.p(_crate.weight)))
+            _weight = _weight + _crate.weight
+            _description = _description .. string.format("1 %s crate onboard (%s kg)\n", _crate.desc, _crate.weight)
         --end
     end
     ctld.logTrace(string.format("with simulated slingload crates : weight = %s", tostring(_weight)))
