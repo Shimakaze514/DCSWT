3ff05c8548eaed17289e135f3442f8fbfb82f60e
Meowrasaki <shimakaze001@outlook.com>
2025-09-20 14:59:25 +0800

    更新爆破函数的debug信息

--- Mission/AddEXPL.lua
+++ Mission/AddEXPL.lua
@@ -5,13 +5,17 @@ function AddEXPL.eventHandler:onEvent(_event)
         if not _event or not _event.id then return end
         if _event.id == world.event.S_EVENT_HIT then
             if _event.target and _event.target:getTypeName() == "CH-47Fbl1" then
+                env.info("[AddEXPL] Info: 击中CH47的事件触发")
                 if _event.weapon ~= nil then
+                    env.info("[AddEXPL] Info: 成功找到击中CH47的武器")
                     local weaponDesc = _event.weapon:getDesc()
                     if weaponDesc then
-                        env.info("[AddEXPL] Info: 击中CH47的武器是: " .. ctld.formatTable(weaponDesc))
+                        env.info("[AddEXPL] Info: 成功获取描述，击中CH47的武器是: " .. ctld.formatTable(weaponDesc))
                         local explMass = 100
                         if weaponDesc.warhead and weaponDesc.warhead.explosiveMass then
-                            explMass = weaponDesc.warhead.explosiveMass * 10
+                            explMass = weaponDesc.warhead.explosiveMass * 12
+                            env.info("[AddEXPL] Info: 使用武器的当量设置爆炸，原武器当量: " .. weaponDesc.warhead.explosiveMass .. "，设置爆炸当量: " .. explMass)
+                        else
                         end
                         trigger.action.explosion(_event.target:getPoint(),explMass)
                     else
