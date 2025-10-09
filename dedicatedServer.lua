b74c716aa45de3f26322b9175cb11934f6ef170b
Meowrasaki <shimakaze001@outlook.com>
2025-10-09 19:39:46 +0800

    自动覆写name

--- dedicatedServer.lua
+++ dedicatedServer.lua
@@ -1,4 +1,10 @@
+local net = require('net')
+local lfs = require('lfs')
 net.set_name('ThunderDCS')
+local mpConfig = dofile(lfs.writedir()..'Config/serverSettings.lua')
+if mpConfig and mpConfig.cfg then
+  mpConfig.cfg["name"] = "：：：【TD动态战役】空地联合PvP现代战场：：："
+end
 local res = net.start_server(serverSettings)
 if res ~= 0 then
   log.write('专用服务器', log.DEBUG, '无法以代码启动服务器:', res)
