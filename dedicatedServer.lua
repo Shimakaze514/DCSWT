321acefba0228fe2e4acdf41f4f04208119715d9
Meowrasaki <shimakaze001@outlook.com>
2025-10-11 14:10:34 +0800

    换一种自动覆写name的方法

--- dedicatedServer.lua
+++ dedicatedServer.lua
@@ -1,9 +1,6 @@
 net.set_name('ThunderDCS')
-local mpConfig = dofile(lfs.writedir()..'Config/serverSettings.lua')
-if mpConfig and mpConfig.cfg then
-  mpConfig.cfg["name"] = "：：：【TD动态战役】空地联合PvP现代战场：：："
-end
-local res = net.start_server(mpConfig.cfg)
+serverSettings["name"] = "：：：【TD动态战役】空地联合PvP现代战场：：："
+local res = net.start_server(serverSettings)
 if res ~= 0 then
   log.write('专用服务器', log.DEBUG, '无法以代码启动服务器:', res)
 end
