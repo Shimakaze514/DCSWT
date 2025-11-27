6912f80ffd0acc081a02ed880a708d2daea4f7eb
Meowrasaki <shimakaze001@outlook.com>
2025-11-27 21:42:19 +0800

    modified addexpl for corrected weapon object

--- Mission/Modules/AddEXPL.lua
+++ Mission/Modules/AddEXPL.lua
@@ -19,23 +19,22 @@ function AddEXPL.eventHandler:onEvent(_event)
                     trigger.action.explosion(_event.target:getPoint(), 80)
                     return
                 end
+                if _event.weapon and type(_event.weapon) == 'table' and _event.weapon.isExist and _event.weapon:isExist() then
+                    local weaponObj = _event.weapon
+                    env.info('[AddEXPL] Info: 成功找到击毁目标的武器')
+                    local weaponDesc = weaponObj.getDesc and weaponObj:getDesc()
+                    if weaponDesc then
+                        env.info('[AddEXPL] Info: 成功获取描述，击毁目标的武器是: ' .. ctld.formatTable(weaponDesc))
+                    else
+                        env.info('[AddEXPL] Info: 击毁目标的武器没有WeaponDesc')
+                    end
+                else
+                    env.info('[AddEXPL] Info: 击毁目标的武没有Weapon对象信息')
+                end
 				local life = _event.target:getLife() / _event.target:getLife0()
 				if life <= 0.5 then
 					env.info('[AddEXPL] Info: 单位血量小于爆炸临界，血量分数(0,1)为' .. life)
-					if _event.weapon and type(_event.weapon) == 'table' and _event.weapon.isExist and _event.weapon:isExist() then
-						local weaponObj = _event.weapon
-						env.info('[AddEXPL] Info: 成功找到击毁目标的武器')
-						local weaponDesc = weaponObj.getDesc and weaponObj:getDesc()
-						if weaponDesc then
-							env.info('[AddEXPL] Info: 成功获取描述，击毁目标的武器是: ' .. ctld.formatTable(weaponDesc))
-						else
-							env.info('[AddEXPL] Info: 击毁目标的武器没有WeaponDesc')
-						end
-						trigger.action.explosion(_event.target:getPoint(), 50)
-					else
-						env.info('[AddEXPL] Info: 击毁目标的武没有Weapon对象信息')
-						trigger.action.explosion(_event.target:getPoint(), 50)
-					end
+                    trigger.action.explosion(_event.target:getPoint(), 50)
 				end
 			end
 		end
