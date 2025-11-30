db17a66effdbdab6929c7f8a4469846d521f42ea
Meowrasaki <shimakaze001@outlook.com>
2025-11-30 10:24:42 +0800

    update auto restart

--- Hooks/DCS-AutoRestartOnMissionTimeGameGUI.lua
+++ Hooks/DCS-AutoRestartOnMissionTimeGameGUI.lua
@@ -1,12 +1,13 @@
 -- Auto-retart script from Asta, if any question, find me here: https://discord.gg/ZUZdMzQ
-net.log("Begin script auto-restart server")
+net.log("[auto-restart] Begin script auto-restart server")
 
 local SW = {}
 
 function SW.onPlayerDisconnect(reason_msg, err_code)
     if DCS.getModelTime() > 28800 then -- Number of seconds
+        net.log("[auto-restart] It's time! restarting if possible")
         local listPlayers = net.get_player_list()
-        if #listPlayers == 1 then --1 because the dedicated server is also returned
+        if #listPlayers <= 2 then --1 because the dedicated server is also returned
             net.load_mission(DCS.getMissionFilename())
         end
     end
@@ -14,6 +15,6 @@ end
 
 DCS.setUserCallbacks(SW)
  
-net.log("End script auto-restart server")
+net.log("[auto-restart] End script auto-restart server")
 
 -- put it here > C:\Users\yourNickname\Saved Games\DCS.openbeta\Scripts\Hooks
