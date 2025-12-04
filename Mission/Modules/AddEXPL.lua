a1f67ee6f679c3bada8cedc72e1e549b32ddff6f
Meowrasaki <shimakaze001@outlook.com>
2025-12-05 02:20:03 +0800

    added delay to addExpl to let it function normally

--- Mission/Modules/AddEXPL.lua
+++ Mission/Modules/AddEXPL.lua
@@ -9,42 +9,53 @@ function AddEXPL.eventHandler:onEvent(_event)
 				return
 			end
 			if _event.id == world.event.S_EVENT_HIT and _event.target and _event.target.getDesc then
-                local life = _event.target:getLife() / _event.target:getDesc().life
-                env.info('[AddEXPL] Info: 当前血量'.._event.target:getLife().."，最大血量".._event.target:getDesc().life.."，血量分数"..life)
-                if _event.target:getLife() <= 1 then
-                    trigger.action.explosion(_event.target:getPoint(), 80)
-                    env.info('[AddEXPL] Info:单位血量小于等于1，执行爆破')
-                elseif _event.weapon_name and _event.weapon_name ~= "" then
-                    local weaponTypeName = _event.weapon_name
-                    if weaponTypeName then
+                timer.scheduleFunction(
+                function(_args)
+                    local target, weapon = _args[1], _args[2]
+                    if not target or not target:isExist() then
+                        env.info('[AddEXPL] Warning: 单位在定时器执行前已被摧毁或不可用。')
+                        return
+                    end
+                    local currentHP = target:getLife()
+                    local maxLife = target:getDesc().life
+                    local lifeRatio = currentHP / maxLife
+                    if currentHP <= 1 then
+                        trigger.action.explosion(target:getPoint(), 80)
+                        env.info('[AddEXPL] Info:单位血量小于等于1，执行爆破')
+                    elseif weapon and weapon ~= "" then
+                        local weaponTypeName = weapon
                         env.info('[AddEXPL] Info: 击中目标的武器是: ' .. weaponTypeName)
                         local warheadMass = AddEXPL.warheadMass[weaponTypeName]
+                        
                         if warheadMass then
-                            if _event.target:getDesc().category == Unit.Category.AIRPLANE or
-                                _event.target:getDesc().category == Unit.Category.HELICOPTER
+                            local unitCategory = target:getDesc().category
+                            
+                            if unitCategory == Unit.Category.AIRPLANE or
+                            unitCategory == Unit.Category.HELICOPTER
                             then
-                                if life <= 0.5 then
-                                    env.info('[AddEXPL] Info: 单位血量小于爆炸临界0.5，血量分数(0,1)为' .. life)
-                                    trigger.action.explosion(_event.target:getPoint(), 50)
+                                if lifeRatio <= 0.5 then
+                                    env.info('[AddEXPL] Info: 单位血量小于爆炸临界0.5，血量分数(0,1)为' .. lifeRatio)
+                                    trigger.action.explosion(target:getPoint(), 50)
                                 end
-                                if _event.target:getTypeName() == "CH-47Fbl1" then
+                                if target:getTypeName() == "CH-47Fbl1" then
                                     env.info('[AddEXPL] Info: CH-47Fbl1被击中')
-                                    trigger.action.explosion(_event.target:getPoint(), warheadMass * 10)
+                                    trigger.action.explosion(target:getPoint(), warheadMass * 10)
                                     return
                                 end
-                            elseif life <= 0.1 then
-                                env.info('[AddEXPL] Info: 单位是地面或海面目标，单位血量小于爆炸临界0.1，血量分数(0,1)为' .. life)
-                                trigger.action.explosion(_event.target:getPoint(), 100)
+                            elseif lifeRatio <= 0.1 then
+                                env.info('[AddEXPL] Info: 单位是地面或海面目标，单位血量小于爆炸临界0.1，血量分数(0,1)为' .. lifeRatio)
+                                trigger.action.explosion(target:getPoint(), 100)
                             end
                         else
                             env.info('[AddEXPL] Info: AddEXPL.warheadMass中没有对应的weaponTypeName')
                         end
                     else
-                        env.info('[AddEXPL] Info: 击中目标的武器没有WeaponDesc')
+                        env.info('[AddEXPL] Info: 击中目标的武器没有Weapon对象信息')
                     end
-                else
-                    env.info('[AddEXPL] Info: 击中目标的武器没有Weapon对象信息')
-                end
+                end,
+                {_event.target,_event.weapon_name},
+                timer.getTime() + 0.1
+                )
 			elseif _event.id == world.event.S_EVENT_SHOT and _event.weapon and _event.weapon.isExist and _event.weapon:isExist() and Object.getCategory(_event.weapon) == Object.Category.WEAPON then 
                 local weaponObj = _event.weapon
                 local weaponTypeName = weaponObj:getTypeName()
