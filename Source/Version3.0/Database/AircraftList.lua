34f3c994ea5f84549a721a9e252336c99ddee325
Meowrasaki <shimakaze001@outlook.com>
2025-12-07 16:55:22 +0800

    updated aircraft list

--- Source/Version3.0/Database/AircraftList.lua
+++ Source/Version3.0/Database/AircraftList.lua
@@ -2,7 +2,9 @@ NPAircraftList = {}
 NPAircraftList.superiorityFighter = {"F-15C", "J-11A", "Su-33",  "FA-18C_hornet", "JF-17", "F-14B", "F-14A", "F-16C_50","F-15ESE", "F-14A-135-GR", "F-14A-95-GR", "C-130J-30"}
 NPAircraftList.superiorityFighterPoint = 100
 
-NPAircraftList.lightFighter = {"Su-27", "MiG-29S", "MiG-29G", "MiG-29A",'MiG-29 Fulcrum', "M-2000C", "AJS37", "MiG-21Bis", "MiG-15bis_FC", "MiG-15bis", "MiG-19P", "F-4E-45MC", "F-5E-3", "F-5E-3_FC", "F-86F Sabre", "F-86F_FC", "AV8BNA"}
+NPAircraftList.lightFighter = {"Su-27", "MiG-29S", "MiG-29G", "MiG-29A",'MiG-29 Fulcrum', "MiG-21Bis", "MiG-15bis_FC", "MiG-15bis", "MiG-19P",
+                            "M-2000C", "AJS37", "Mirage-F1B", "Mirage-F1BD", "Mirage-F1BE", "Mirage-F1BQ", "Mirage-F1DDA", "Mirage-F1EE",
+                            "F-4E-45MC", "F-5E-3", "F-5E-3_FC", "F-86F Sabre", "F-86F_FC", "AV8BNA", }
 NPAircraftList.lightFighterPoint = 80
 
 NPAircraftList.attacker = {"A-10A", "A-10C", "Ka-50", "Ka-50_3","AH-64D_BLK_II", "A-10C_2", "AV-8B", "Su-25", "Su-25T","SA342Mistral"}
